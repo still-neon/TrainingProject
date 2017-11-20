f9d0e9e741a9ee9e66802a80ae0cf669d703bf06
Eugene Krasotkin <still-neon@tut.by>
2017-11-20 18:28:43 +0300

    initial commit 1

--- src/main/java/by/stn/trainingproject/counter/console/InputConsole.java
+++ src/main/java/by/stn/trainingproject/counter/console/InputConsole.java
@@ -1,8 +1,8 @@
 package by.stn.trainingproject.counter.console;
 
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class InputConsole {
     public static void main(String[] args) {
