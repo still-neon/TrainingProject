355814f17e2047bbe21f98a8a0c58da2e662143c
Eugene Krasotkin <still-neon@tut.by>
2017-11-29 16:49:14 +0300

    added files

---
+++ src/main/java/by/stn/callslogproject/CallsLogger.java
@@ -0,0 +1,17 @@
+package by.stn.callslogproject;
+
+/**
+ * Created by EugenKrasotkin on 11/29/2017.
+ */
+public class CallsLogger {
+    private String callerName;
+    private String adresseeName;
+    //add
+
+    private enum CallType {
+        INCOMMING, OUTGOING, CONFERENCE
+    }
+
+    private String callStartDate;
+    private String callEndDate;
+}
---
+++ src/main/java/by/stn/callslogproject/SubscribersList.java
@@ -0,0 +1,12 @@
+package by.stn.callslogproject;
+
+/**
+ * Created by EugenKrasotkin on 11/29/2017.
+ */
+public class SubscribersList {
+    private String subscriberName;
+    private String organizationName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String creationDate;
+}
