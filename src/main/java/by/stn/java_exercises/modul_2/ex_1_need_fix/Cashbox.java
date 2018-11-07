f7c8d227aa413f3240884ae5c56925317c51c13b
eugenkrasotkin <105222>
2018-11-07 11:53:22 +0300

    Added db work class

--- src/main/java/by/stn/java_exercises/modul_2/ex_1_need_fix/Cashbox.java
+++ src/main/java/by/stn/java_exercises/modul_2/ex_1_need_fix/Cashbox.java
@@ -1,11 +1,31 @@
 package by.stn.java_exercises.modul_2.ex_1_need_fix;
 
-/**
- * Created by EugenKrasotkin on 3/29/2018.
- */
-public class Cashbox {
-    public void printGoods(Customer customer) {
-        for(Goods good: customer.getGoods())
-        System.out.println(good.getName());
-    }
+import lombok.Getter;
+
+public class CashBox {
+	@Getter
+	private boolean free;
+	private int number = (int) (Math.random() * 9999); //чисто для проверки какая касса обслуживает, потом удалю
+
+	public CashBox() {
+		unlock();
+	}
+
+	public synchronized void serve(Customer customer) {
+		//lock(); пока не доделал реализацию
+		System.out.println("CashBox number " + number + " Customers goods are:");
+		for (Goods good : customer.getGoods()) {
+			System.out.println(good.getName());
+		}
+		customer.makeServed();
+		//unlock();
+	}
+
+	private void lock() {
+		free = false;
+	}
+
+	private void unlock() {
+		free = true;
+	}
 }
