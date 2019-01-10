ead1e3805c3e06b7eae51b2e0c6635963212ad6b
eugenkrasotkin <105222>
2019-01-10 17:55:20 +0300

    Added db work class

--- src/main/java/by/stn/callslogproject/ui/UICreator.java
+++ src/main/java/by/stn/callslogproject/ui/UICreator.java
@@ -5,79 +5,56 @@ import lombok.Setter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class UICreator {
 	private static final String APP_NAME = "CallsLog";
-	private static final String ADD_BUTTON_TEXT = "Add Call";
-	private static final String SAVE_BUTTON_TEXT = "Save";
-	private static final String REFRESH_BUTTON_TEXT = "Refresh";
-	private static final String DELETE_BUTTON_TEXT = "Delete Call";
+	private static final String ADD_BUTTON_LABEL = "Add Call";
+	private static final String SAVE_BUTTON_LABEL = "Save";
+	private static final String REFRESH_BUTTON_LABEL = "Refresh";
+	private static final String DELETE_BUTTON_LABEL = "Delete Call";
 	@Setter
 	private CallsLogTableManager callsLogTableManager;
 
 	public void create() {
 		SwingUtilities.invokeLater(new Runnable() {
-			private JButton addButton = new JButton(ADD_BUTTON_TEXT);
-			private JButton deleteButton = new JButton(DELETE_BUTTON_TEXT);
-			private JButton saveButton = new JButton(SAVE_BUTTON_TEXT);
-			private JButton refreshButton = new JButton(REFRESH_BUTTON_TEXT);
-			private JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
-			private JFrame frame = new JFrame(APP_NAME);
+			JFrame frame;
 
 			public void run() {
-				configure();
-				addListeners();
+				frame = createFrame();
+				frame.add(createContents(), BorderLayout.NORTH);
+				frame.add(createScrollPane(), BorderLayout.CENTER);
 			}
 
-			private void configure() {
+			private JFrame createFrame() {
+				frame = new JFrame(APP_NAME);
 				frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 				frame.setSize(700, 600);
 				frame.setResizable(true);
 				frame.setVisible(true);
 				frame.setLayout(new BorderLayout());
-				frame.add(contents, BorderLayout.NORTH);
-
-				contents.add(addButton);
-				contents.add(deleteButton);
-				contents.add(saveButton);
-				contents.add(refreshButton);
-
-				frame.add(new JScrollPane(callsLogTableManager.getTable()), BorderLayout.CENTER);//создавать таблицу здесь и передавать
+				return frame;
 			}
 
-			private void addListeners() {
-				addButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						callsLogTableManager.addRow();
+			private JPanel createContents() {
+				JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				contents.add(createButton(ADD_BUTTON_LABEL, e -> callsLogTableManager.addRow()));
+				contents.add(createButton(DELETE_BUTTON_LABEL, e -> callsLogTableManager.deleteRow()));
+				contents.add(createButton(SAVE_BUTTON_LABEL, e -> callsLogTableManager.save()));
+				contents.add(createButton(REFRESH_BUTTON_LABEL, e -> callsLogTableManager.refresh()));
+				return contents;
+			}
 
-					}
-				});
+			private JButton createButton(String label, ActionListener e) {
+				JButton button = new JButton(label);
+				button.addActionListener(e);
+				return button;
+			}
 
-				deleteButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						callsLogTableManager.deleteRow();
-					}
-				});
-
-				saveButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						callsLogTableManager.save();
-					}
-				});
-
-				refreshButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						callsLogTableManager.refresh();
-					}
-				});
+			private JScrollPane createScrollPane() {
+				return new JScrollPane(callsLogTableManager.getTable());
 			}
 		});
 	}
