488500996135f2515b5f58ba2232f825a55698ac
eugenkrasotkin <105222>
2019-03-18 18:40:28 +0300

    Added db work class

--- src/main/java/by/stn/data_parser/parser/DataParser.java
+++ src/main/java/by/stn/data_parser/parser/DataParser.java
@@ -1,13 +1,21 @@
 package by.stn.data_parser.parser;
 
 import by.stn.data_parser.tokens.Token;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.util.List;
 
 public class DataParser {
 
 	public List<Token> getParsedJSONData(String data) {
-
+		try {
+			JSONArray jo = (JSONArray) new JSONParser().parse(data);
+			System.out.println(jo);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
